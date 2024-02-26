7dfaa1feafb60ad297ea174f41595f11cfacd231
Jonatan Kwiatkowski <Joniqs91@gmail.com>
2024-02-26 10:54:09 +0000

    Added Error handling, asyncHandler for follow DRY principles.

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
-// Route files
-const bootcamps = require('./routes/bootcamps');
-
 // Connect to database
 connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -25,6 +26,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+// Error Handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 

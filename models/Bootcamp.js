89eedbbabae0933b02e63d8c16f60fafc3e36651
Jonatan Kwiatkowski <Joniqs91@gmail.com>
2024-02-28 16:12:47 +0000

    Added CRUD operations to courses database.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -134,10 +134,14 @@ BootcampSchema.pre('save', async function (next) {
 });
 
 // Cascade delete courses when a bootcamp is deleted
-BootcampSchema.pre('remove', async function (next) {
-  await this.model('Course').deleteMany({ bootcamp: this._id });
-  next();
-});
+BootcampSchema.pre(
+  'deleteOne',
+  { document: true, query: false },
+  async function (next) {
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+  }
+);
 
 // Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
